1de0ee3474d805a70f77b8c585e92e1e54338cf7
borfus <mrpeteyb@gmail.com>
2022-12-11 14:02:33 -0600

    Added additional logic for there to be a 1 in 500 chance to get a shiny pokemon while using luckymon command.

--- src/commands/luckymon.rs
+++ src/commands/luckymon.rs
@@ -14,18 +14,18 @@ use chrono::NaiveDate;
 pub struct LuckymonDailyEntry;
 
 impl TypeMapKey for LuckymonDailyEntry {
-    type Value = HashMap<(UserId, NaiveDate), i64>;
+    type Value = HashMap<(UserId, NaiveDate), (i64, bool)>;
 }
 
-async fn write_daily_entry(ctx: &Context, user_id: UserId, lucky_num: i64) {
+async fn write_daily_entry(ctx: &Context, user_id: UserId, lucky_num: i64, shiny: bool) {
     let today = Timestamp::now().date_naive();
     let mut data = ctx.data.write().await;
     let daily_entry = data.get_mut::<LuckymonDailyEntry>().unwrap();
-    let entry = daily_entry.entry((user_id, today)).or_insert(lucky_num);
-    *entry = lucky_num;
+    let entry = daily_entry.entry((user_id, today)).or_insert((lucky_num, shiny));
+    *entry = (lucky_num, shiny);
 }
 
-async fn read_daily_entry(ctx: &Context, user_id: UserId) -> Option<i64> {
+async fn read_daily_entry(ctx: &Context, user_id: UserId) -> Option<(i64, bool)> {
     let today = Timestamp::now().date_naive();
     let data = ctx.data.read().await;
     let daily_entry = data.get::<LuckymonDailyEntry>().unwrap();
@@ -43,24 +43,41 @@ async fn luckymon(ctx: &Context, msg: &Message) -> CommandResult {
     println!("Got luckymon command..");
     let user_id = msg.author.id;
 
-    let mut lucky_num = fastrand::i64(1..=905);
-    if let Some(num) = read_daily_entry(ctx, user_id).await {
-        lucky_num = num;
+    let lucky_num = fastrand::i64(1..=905);
+    let shiny_num = fastrand::i64(1..=500);
+    let mut is_shiny = false;
+    if shiny_num == 1 {
+        is_shiny = true;
+    }
+
+    let mut daily_pair: (i64, bool) = (lucky_num, is_shiny);
+
+    if let Some((num, shiny)) = read_daily_entry(ctx, user_id).await {
+        daily_pair = (num, shiny);
     } else {
-        write_daily_entry(ctx, user_id, lucky_num).await;
+        write_daily_entry(ctx, user_id, lucky_num, is_shiny).await;
     }
 
     let rustemon_client = RustemonClient::default();
     let lucky_pokemon: Pokemon = pokemon::get_by_id(lucky_num, &rustemon_client).await?;
-    let capitalized_name = lucky_pokemon.name[0..1].to_uppercase() + &lucky_pokemon.name[1..];
+
+    let regular_name = lucky_pokemon.name[0..1].to_uppercase() + &lucky_pokemon.name[1..];
+    let mut final_name = String::from(&regular_name);
+    let regular_sprite = lucky_pokemon.sprites.front_default.unwrap();
+    let shiny_sprite = lucky_pokemon.sprites.front_shiny.unwrap();
+    let mut sprite = regular_sprite;
+    if daily_pair.1 {
+        final_name = format!("Shiny {}", regular_name);
+        sprite = shiny_sprite;
+    }
 
     let _msg = msg
         .channel_id
         .send_message(&ctx.http, |m| {
             m.embed(|e| {
                 e.title("You lucky pokemon of the day is:")
-                    .image(lucky_pokemon.sprites.front_default.unwrap().as_str())
-                    .fields(vec!((format!("{}!", &capitalized_name), format!("[Bulbapedia Page](https://bulbapedia.bulbagarden.net/wiki/{}_(Pok%C3%A9mon))", capitalized_name).to_string(), false)))
+                    .image(sprite)
+                    .fields(vec!((format!("{}!", &final_name), format!("[Bulbapedia Page](https://bulbapedia.bulbagarden.net/wiki/{}_(Pok%C3%A9mon))", regular_name).to_string(), false)))
                     .timestamp(Timestamp::now())
             })
         })
